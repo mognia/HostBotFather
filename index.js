d8fc89bf97eb701b2824f90d5a5cc0f0b9f9b9f8
mognia <mognia420@gmail.com>
2019-01-02 23:32:06 +0330

    Login / bot List / add Bot

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 process.env["NTBA_FIX_319"] = 1;
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config()
+require('dotenv').config();
+var EventEmitter = require('events');
+var eventEmitter = new EventEmitter();
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:27017/hostFather";
-
-
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+let isAuth = false;
+let LogedUser;
 function CreateUser(tempUsername, tempNum, msg) {
     // console.log(`${tempUsername} : ${tempNum}`);
 
@@ -24,7 +28,7 @@ function CreateUser(tempUsername, tempNum, msg) {
                     }
                 });
                 db.close();
-            } 
+            }
             if (result.length == 0) {
                 //if user name is not exist in next step checking Phone existing
                 dbo.collection("users").find({ Phone: tempNum }).toArray(function (err, result) {
@@ -48,7 +52,7 @@ function CreateUser(tempUsername, tempNum, msg) {
                             // }
                             bot.sendMessage(msg.chat.id, "cool now you are one of us!", {
                                 "reply_markup": {
-                                    "keyboard": [["/login", "New Register Form"]],
+                                    "keyboard": [["/login"]],
                                     "one_time_keyboard": true,
                                 }
                             });
@@ -67,18 +71,117 @@ function CreateUser(tempUsername, tempNum, msg) {
 
 }
 
-const token = process.env.BOT_TOKEN;
+AuthNumber = '123';
 
+let LogedPhone;
+function AuthUser(tempUsername, tempNum, msg) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db('hostFather');
+        dbo.collection("users").find({ Phone: tempNum }).toArray(function (err, result) {
+            //if Phone number not exist
 
+            if (result.length == 0) {
+                bot.sendMessage(msg.chat.id, "hey, i think still i don't have This Phone :( \nif you'r not registred yet you can doit now!", {
+                    "reply_markup": {
+                        "keyboard": [["/register"], ['/login']],
+                        "one_time_keyboard": true,
+                    }
+                });
+                db.close();
+            } else {
+                LogedPhone = tempNum;
+                bot.sendMessage(msg.chat.id, AuthTxt, { reply_markup: JSON.stringify({ force_reply: true }) }, {
+                    "reply_markup": {
+                        "keyboard": [["Resend The code!"], ['/login']],
+                        "one_time_keyboard": true,
+                    }
+                });
+            }
+        });
 
-const bot = new TelegramBot(token, { polling: true });
-//last pm that sent to user
-let lastPm;
-//meg to get username from user
+    });
+
+}
+
+function Landing(msg) {
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        var dbo = db.db('hostFather');
+        dbo.collection("users").findOne({ Phone: LogedPhone }, function (err, result) {
+            if (err) throw err;
+            // console.log(result);
+            LogedUser = result;
+            isAuth = true;
+            bot.sendMessage(msg.chat.id, `Welcome back dear ${result.userName}`, {
+                "reply_markup": {
+                    "keyboard": [["Add New Bot", "Delete Bot", "Edit Bot"], ["My Bots"]],
+                }
+            });
+            db.close();
+        });
+    });
+}
+
+function CreatBot(NewBotName, NewBotKey, msg) {
+    let newBot = { name: NewBotName, owner: LogedUser.userName, Key: NewBotKey }
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        var dbo = db.db('hostFather');
+        //adding new User to DB
+        dbo.collection('bots').insertOne(newBot, function (err, res) {
+            // if (err) {
+            //     console.log(err);
+
+            // }
+            bot.sendMessage(msg.chat.id, `${NewBotName} Succesfully Added! ^_^`, {
+                "reply_markup": {
+                    "keyboard": [["/login"]],
+                    "one_time_keyboard": true,
+                }
+            });
+            console.log(`${NewBotName} added to db`);
+            db.close();
+
+        });
+    });
+}
+function ListBots(msg) {
+
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
+        var dbo = db.db('hostFather');
+        //Find Bots that Loged user Have!
+        dbo.collection("bots").find({ owner: LogedUser.userName }).toArray(function (err, result) {
+            if (err) throw err;
+            let botNames=[]
+            for (const i of result) {
+                botNames.push(i.name)
+            }
+            
+            let keyBoard = [];
+            for (let i = 0; i < botNames.length; i++) {
+                keyBoard.push([{'text':botNames[i],'callback_data':botNames[i]}])
+            }
+            
+            bot.sendMessage(msg.message.chat.id, `Corrently You have ${botNames.length} bots`, {
+                "reply_markup": {
+                    "inline_keyboard": keyBoard
+                }
+            });
+        });
+    });
+}
+
+//mesg to get username from user
 let UsernameLabel = 'Please Enter your User name:';
 let NumLabel = 'Please Enter your Phone Number:';
+let UserLoginLabel = 'User name:';
+let PassLoginLabel = 'Phone Number:';
+let AuthTxt = "i sent a Authentication number to you'r phone \ntell me what is that?!";
+let addBotName = 'ok what should i call your New Bot?!';
+let addBotKey = 'nice name! now give me your Bot Key\n that key when you make new bot the BotFather gives to you....'
 let tempUsername = '';
 let tempNum = '';
+let NewBotName;
+let NewBotKey;
 bot.onText(/\/start/, (msg) => {
     //welcome msg and defualt keyboard
     bot.sendMessage(msg.chat.id, "Welcome", {
@@ -89,33 +192,64 @@ bot.onText(/\/start/, (msg) => {
     });
 
 });
-//if user selected /Register
-bot.onText(/\/register/, (msg) => {
-
-    bot.sendMessage(msg.chat.id,
-        UsernameLabel,
-        { reply_markup: JSON.stringify({ force_reply: true }) })
-        .then(rep => {
-            //update the last pm state
-            lastPm = rep;
+bot.onText(/My Bots/, (msg) => {
+    if (!isAuth) {
+        // err 503
+        bot.sendMessage(msg.chat.id, "ok but\nYou need To Login or Register first", {
+            "reply_markup": {
+                "keyboard": [["/login"], ["/register"]],
+                "one_time_keyboard": true,
+            }
         });
 
+    } else {
+        bot.sendMessage(msg.chat.id, `Welcome back dear ${LogedUser.userName}`, {
+            "reply_markup": {
+                "inline_keyboard": [
+                    [{ text: "Your Bots", callback_data: 'my_bots' }], // Clicking will send "my_bots"
+                ]
+            }
+        });
+    }
 
+});
+//if user selected /Register
+bot.onText(/\/register/, (msg) => {
+    
+    bot.sendMessage(msg.chat.id,
+        UsernameLabel,
+        { reply_markup: JSON.stringify({ force_reply: true }) });
 });
 bot.onText(/New Register Form/, (msg) => {
 
     bot.sendMessage(msg.chat.id,
         UsernameLabel,
         { reply_markup: JSON.stringify({ force_reply: true }) })
-        .then(rep => {
-            //update the last pm state
-            lastPm = rep;
-        });
-
 
 });
 bot.onText(/\/login/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Welcome");
+    bot.sendMessage(msg.chat.id,
+        UserLoginLabel,
+        { reply_markup: JSON.stringify({ force_reply: true }) });
+});
+bot.onText(/Add New Bot/, (msg) => {
+    if (!isAuth) {
+        // err 503
+        bot.sendMessage(msg.chat.id, "ok but\nYou need To Login or Register first", {
+            "reply_markup": {
+                "keyboard": [["/login"], ["/register"]],
+                "one_time_keyboard": true,
+            }
+        });
+
+    }
+    else{
+        bot.sendMessage(msg.chat.id,
+            addBotName,
+            { reply_markup: JSON.stringify({ force_reply: true }) })
+    }
+
+
 });
 //if user sent any message
 bot.on('message', (msg) => {
@@ -130,7 +264,6 @@ bot.on('message', (msg) => {
 
             let msgText = msg.text
             if (msgText.length < 3) {
-                console.log(msgText.length);
 
                 bot.sendMessage(msg.chat.id, "User name Should contain more than 3 letter!", {
                     "reply_markup": {
@@ -145,10 +278,7 @@ bot.on('message', (msg) => {
                 bot.sendMessage(msg.chat.id,
                     NumLabel,
                     { reply_markup: JSON.stringify({ force_reply: true }) })
-                    .then(rep => {
-                        //update the last pm state
-                        lastPm = rep;
-                    });
+
             }
         }
         if (msg.reply_to_message.text == NumLabel) {
@@ -172,13 +302,69 @@ bot.on('message', (msg) => {
             }
 
         }
+        //if user sent username to loging in
+        if (msg.reply_to_message.text == UserLoginLabel) {
+            tempUsername = msg.text;
+            bot.sendMessage(msg.chat.id,
+                PassLoginLabel,
+                { reply_markup: JSON.stringify({ force_reply: true }) })
+        }
+        //if user sent pass to loginig in
+        if (msg.reply_to_message.text == PassLoginLabel) {
 
+
+            tempNum = msg.text;
+            AuthUser(tempUsername, tempNum, msg);
+
+        }
+
+        //if user sent Auth code to loginig in
+        if (msg.reply_to_message.text == AuthTxt) {
+
+            if (msg.text == AuthNumber) {
+
+                Landing(msg);
+            }
+            else {
+                bot.sendMessage(msg.chat.id, 'This is not that code i sent to you!\n', {
+                    "reply_markup": {
+                        "keyboard": [["Resend The code!"], ['/login']],
+                        "one_time_keyboard": true,
+                    }
+                });
+            }
+
+        }
+
+
+        //if user want to sent new bot name
+        if (msg.reply_to_message.text == addBotName) {
+            NewBotName = msg.text;
+
+
+            bot.sendMessage(msg.chat.id,
+                addBotKey,
+                { reply_markup: JSON.stringify({ force_reply: true }) })
+        }
+        //if user want to sent new bot key
+        if (msg.reply_to_message.text == addBotKey) {
+            console.log(NewBotName);
+            NewBotKey = msg.text;
+            CreatBot(NewBotName, NewBotKey, msg);
+        }
     }
     else {
         return;
     }
 
 });
+
+bot.on('callback_query', (msg) => {
+    if (msg.data == 'my_bots') {
+        ListBots(msg);
+    }
+
+})
 bot.on('polling_error', (error) => {
     console.log(error);  // => 'EFATAL'
 });
